006e95dd5d4172617ac90cfc53905e1bed474550
sugiralieva <zhansaya.sugiralieva@gmail.com>
2024-04-07 16:49:16 +0500

    feat: pages for news, articles and news about kazakhstan's future

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template, g
 import sqlite3 as sq
 import os
-from articles_db import ArticlesDB
+from tengrinews_db import ArticlesDB
 
 
 SECRET_KEY = 'fdgdfgdfggf786hfg6hfg6h7f'
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 app.config.from_object(__name__)
-app.config.update(dict(DATABASE=os.path.join(app.root_path, 'instance/articles_tengrinews.db')))
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'instance/tengrinews.db')))
 
 
 def connect_db():
@@ -43,15 +43,44 @@ def close_db(error):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    news = dbase.get_news_announcement()
+
+    return render_template('index.html', news=news)
+
+
+@app.route('/news/<int:news_id>', methods=['GET', 'POST'])
+def news(news_id):
+    full_news = dbase.get_full_news(news_id)
+
+    return render_template('news.html', full_news=full_news)
+
+
+@app.route('/articles', methods=['GET', 'POST'])
+def articles():
     articles = dbase.get_articles_announcement()
 
-    return render_template('index.html', articles=articles)
+    return render_template('articles.html', articles=articles)
+
 
 @app.route('/article/<int:article_id>', methods=['GET', 'POST'])
 def article(article_id):
     full_article = dbase.get_full_article(article_id)
 
     return render_template('article.html', full_article=full_article)
+
+
+@app.route('/kazakhstan_future', methods=['GET', 'POST'])
+def kazakhstan_future_news():
+    kazakhstan_news = dbase.get_kazakhstan_future_announcement()
+
+    return render_template('kazakhstan_future.html', kazakhstan_news=kazakhstan_news)
+
+
+@app.route('/kazakhstan_future_new/<int:kazakhstan_news_id>', methods=['GET', 'POST'])
+def kazakhstan_future_new(kazakhstan_news_id):
+    kazakhstan_new = dbase.get_full_kazakhstan_future_news(kazakhstan_news_id)
+
+    return render_template('kazakhstan_future_news.html', kazakhstan_new=kazakhstan_new)
 
 
 if __name__ == '__main__':
---
+++ tengrinews_db.py
@@ -0,0 +1,61 @@
+import sqlite3 as sq
+
+
+class ArticlesDB:
+    def __init__(self, db):
+        self.db = db
+        self.cursor = db.cursor()
+
+    def get_news_announcement(self):
+        try:
+            self.cursor.execute('SELECT id, title, image FROM news')
+            res = self.cursor.fetchall()
+            return res
+        except sq.Error as e:
+            print('Ошибка получения новостей' + str(e))
+        return False
+
+    def get_full_news(self, news_id):
+        try:
+            self.cursor.execute('SELECT title, news_text, publication_date, image FROM news WHERE id=?', (news_id,))
+            res = self.cursor.fetchone()
+            return res
+        except sq.Error as e:
+            print('Ошибка получения новости' + str(e))
+        return False
+
+    def get_articles_announcement(self):
+        try:
+            self.cursor.execute('SELECT id, title, image FROM articles')
+            res = self.cursor.fetchall()
+            return res
+        except sq.Error as e:
+            print('Ошибка получения книг по категории' + str(e))
+        return False
+
+    def get_full_article(self, article_id):
+        try:
+            self.cursor.execute('SELECT title, article_text, publication_date, image FROM articles WHERE id=?', (article_id,))
+            res = self.cursor.fetchone()
+            return res
+        except sq.Error as e:
+            print('Ошибка получения книг по категории' + str(e))
+        return False
+
+    def get_kazakhstan_future_announcement(self):
+        try:
+            self.cursor.execute('SELECT id, title, image FROM kazakhstan_future')
+            res = self.cursor.fetchall()
+            return res
+        except sq.Error as e:
+            print('Ошибка получения новостей из категории Что будет с Казахстаном?' + str(e))
+        return False
+
+    def get_full_kazakhstan_future_news(self, news_id):
+        try:
+            self.cursor.execute('SELECT title, news_text, publication_date, image FROM kazakhstan_future WHERE id=?', (news_id,))
+            res = self.cursor.fetchone()
+            return res
+        except sq.Error as e:
+            print('Ошибка получения новости из категории Что будет с Казахстаном?' + str(e))
+        return False
